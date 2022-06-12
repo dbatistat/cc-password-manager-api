437c7be7d3c9a9b034c84cf29df170586d1cda29
David Batista <dbatista.t@gmail.com>
2022-06-12 17:44:06 -0400

    fix secure-password-card.interceptor.ts

--- src/password-card/interceptors/secure-password-card.interceptor.ts
+++ src/password-card/interceptors/secure-password-card.interceptor.ts
@@ -1,18 +1,16 @@
 import { CallHandler, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { PasswordCard, SecurePasswordCard } from '../password-card.types';
+import { PasswordCard } from '../password-card.types';
 
 @Injectable()
 export class SecurePasswordCardInterceptor implements NestInterceptor {
-  intercept(context, next: CallHandler): Observable<SecurePasswordCard> {
+  intercept(context, next: CallHandler): Observable<PasswordCard> {
     return next.handle().pipe(
       map((passwordCards: PasswordCard) => {
         return {
-          id: passwordCards.id,
-          name: passwordCards.name,
-          username: passwordCards.username,
-          url: passwordCards.url,
+          ...passwordCards,
+          password: '***************',
         };
       }),
     );
